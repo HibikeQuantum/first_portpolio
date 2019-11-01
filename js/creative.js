49e965b7cf0aa20c4ed8d6e9ff78ecf684fd0739
Kang Taehoon <jadetypehoon@gmail.com>
2019-11-02 00:26:38 +0900

    aAdd project info

--- js/creative.js
+++ js/creative.js
@@ -58,21 +58,3 @@
 })(jQuery); // End of use strict
 
 var isVisible = false;
-
-$(window).on('scroll',function() {
-  if (checkVisible($('#tester'))&&!isVisible) {
-    isVisible=true;
-    $('#extend').addClass('animate');
-  }
-});
-
-function checkVisible( elm, eval ) {
-  eval = eval || "object visible";
-  var viewportHeight = $(window).height(), // Viewport Height
-      scrolltop = $(window).scrollTop(), // Scroll Top
-      y = $(elm).offset().top,
-      elementHeight = $(elm).height();
-
-  if (eval == "object visible") return ((y < (viewportHeight + scrolltop)) && (y > (scrolltop - elementHeight)));
-  if (eval == "above") return ((y < (viewportHeight + scrolltop)));
-}
